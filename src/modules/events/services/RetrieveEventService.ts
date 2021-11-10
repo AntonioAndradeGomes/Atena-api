680633958dc801911acb96e2a03e3d36993cdca6
Lucas Ferreira <lucasferreiraek@gmail.com>
2021-11-10 01:36:47 -0300

    enhancement(Event): Add error messages when an object couldn't be found or its already exist

--- src/modules/events/services/RetrieveEventService.ts
+++ src/modules/events/services/RetrieveEventService.ts
@@ -1,7 +1,16 @@
+import { AppError } from "../../../errors/AppError";
 import prismaClient from "../../../prisma";
 
 class RetrieveEventService{
   async execute(id: string){
+    const eventAlreadyExists = await prismaClient.event.findFirst({
+      where: {
+        id
+      }
+    });
+
+    if(!eventAlreadyExists) throw new AppError("Event does not exist");
+
     const event = await prismaClient.event.findUnique({
       where: {
         id
