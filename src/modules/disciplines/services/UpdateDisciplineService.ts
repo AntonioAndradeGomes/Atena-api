8b56b4b922d88e34229640c948eb4e0f414121c2
Lucas Ferreira <lucasferreiraek@gmail.com>
2021-11-10 01:30:29 -0300

    enhancement(Discipline): Add message errors when an object was not found

--- src/modules/disciplines/services/UpdateDisciplineService.ts
+++ src/modules/disciplines/services/UpdateDisciplineService.ts
@@ -1,8 +1,17 @@
+import { AppError } from "../../../errors/AppError";
 import prismaClient from "../../../prisma";
 import { Discipline } from "../../../types/discipline";
 
 class UpdateDisciplineService{
   async execute(id: string, {code, name, initials, workload}: Discipline){
+    const disciplineAlreadyExists = await prismaClient.discipline.findFirst({
+      where: {
+        id
+      }
+    });
+
+    if(!disciplineAlreadyExists) throw new AppError("Discipline does not exist");
+    
     const discipline = await prismaClient.discipline.update({
       where: { 
         id 
