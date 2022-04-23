ee8b5b295bc9b8e0c98e570a7c5f9d8e52572694
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2022-04-23 16:38:07 -0300

    feat(route): professor classes

--- src/modules/users/crud/routes/professor.routes.ts
+++ src/modules/users/crud/routes/professor.routes.ts
@@ -2,13 +2,18 @@ import { Router } from "express";
 import { celebrate, Joi, Segments } from "celebrate";
 
 import { ListAllProfessorController } from "../controllers/ListAllProfessorController";
-
+import { ListAllProfessorClassesController } from "../controllers/ListProfessorClassesController";
 
 const listController = new ListAllProfessorController();
+const listClassesController = new ListAllProfessorClassesController();
 
 const professorRoutes = Router();
 
 //listagem de professores
 professorRoutes.get('/', listController.hundle);
 
-export {professorRoutes};
+// List all professor classes
+professorRoutes.get('/classes', listClassesController.handle);
+
+
+export { professorRoutes };
